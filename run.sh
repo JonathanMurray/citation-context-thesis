380966618e22a0bf82d6913de998c4e8255740b3
JonathanMurray <jonte_murray@hotmail.com>
2015-03-07 00:13:09 +0100

    Added trove and commons-lag to classpath, added a variable EXJOBB_IN_TERMINAL to denote whether or not the program is run from a terminal or within eclipse (since eclipse terminal can't handle backspace

--- run.sh
+++ run.sh
@@ -2,9 +2,9 @@
 
 cd bin
 
-export CLASSPATH=".:/opt/commons-lang3-3.3.2:/opt/weka-3-7-12/*:/opt/jsoup/*" 
+export CLASSPATH=".:/opt/commons-lang3-3.3.2:/opt/weka-3-7-12/*:/opt/jsoup/*:/opt/java-trove/3.1a1/lib/*:/opt/commons-lang3-3.3.2/*" 
 export EXJOBB_HOME="/home/jonathan/Documents/eclipse-workspace/exjobb"
 export RESOURCES_DIR="/home/jonathan/Documents/eclipse-workspace/exjobb/resources"
-
+export EXJOBB_IN_TERMINAL="1"
 
 java -Xmx4g main.$1
