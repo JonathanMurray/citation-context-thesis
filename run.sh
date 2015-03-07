4629c1f2009164490f6940c3e94cc64587b4e4f6
JonathanMurray <jonte_murray@hotmail.com>
2015-03-07 03:08:48 +0100

    Added jwi (wordnet)

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 cd bin
 
-export CLASSPATH=".:/opt/commons-lang3-3.3.2:/opt/weka-3-7-12/*:/opt/jsoup/*:/opt/java-trove/3.1a1/lib/*:/opt/commons-lang3-3.3.2/*" 
+export CLASSPATH=".:/opt/commons-lang3-3.3.2:/opt/weka-3-7-12/*:/opt/jsoup/*:/opt/java-trove/3.1a1/lib/*:/opt/commons-lang3-3.3.2/*:/opt/jwi_2.3.3/*" 
 export EXJOBB_HOME="/home/jonathan/Documents/eclipse-workspace/exjobb"
 export RESOURCES_DIR="/home/jonathan/Documents/eclipse-workspace/exjobb/resources"
 export EXJOBB_IN_TERMINAL="1"
