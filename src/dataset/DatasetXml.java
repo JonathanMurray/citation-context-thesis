fcaf9e574f6cd72173432831a5f7952c918c9df4
JonathanMurray <jonte_murray@hotmail.com>
2015-04-14 16:25:48 +0200

    Removed swedish characters in comment

--- src/dataset/DatasetXml.java
+++ src/dataset/DatasetXml.java
@@ -288,7 +288,7 @@ public class DatasetXml {
 			IDictionary dict = new Dictionary(wordnetDir);
 			return (T) TextWithSynsets.fromXml(textTag, dict);
 		}else if(textClass.equals(TextWithSspace.class)){
-			String sspaceDir = Environment.resources() + "/sspace"; //TODO hårdkodade filer här
+			String sspaceDir = Environment.resources() + "/sspace"; //TODO hardcoded paths
 			File sspaceFile = new File(sspaceDir + "/space-lsa.sspace");
 			File wordFrequenciesFile = new File(sspaceDir + "/wordfrequencies.ser");
 			return (T) TextWithSspace.fromXml(textTag, SSpaceWrapper.instance(sspaceFile, wordFrequenciesFile));
